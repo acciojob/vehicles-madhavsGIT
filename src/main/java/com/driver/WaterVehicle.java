d931a41460ceb242f7bdafa3f4e68e428f5efb5c
madhavsGIT <kmadhav455@gamil.com>
2024-04-15 19:26:20 +0530

    p1

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,16 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+class Boat{
+    String name;
+    int capacity;
+
+    public String getVehicleName(String name){
+        this.name = name;
+        return name;
+    }
+    public int getVehicleCapacity(int cap){
+        this.capacity = cap;
+        return cap;
+    }
+}
