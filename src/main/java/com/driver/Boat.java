8aeaf85804b83bb967274294af83eec2228ac492
madhavsGIT <kmadhav455@gamil.com>
2024-04-29 15:31:53 +0530

    finalsub

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,13 +1,26 @@
 package com.driver;
 
-public class Boat implements WaterVehicle{
+public class Boat implements WaterVehicle {
+    private String boatName;
+    private int capacity;
+
+    public Boat(String boatName, int capacity) {
+        this.boatName = boatName;
+        this.capacity = capacity;
+    }
+
+    // Parameterless constructor
+    public Boat() {
+        // You can initialize default values here if needed
+    }
+
     @Override
     public String getVehicleName() {
-        return "";
+        return boatName;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return 0;
+        return capacity;
     }
 }
