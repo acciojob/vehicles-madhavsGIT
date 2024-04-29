b753cff1850ee20c91eccf40416cb13b0f0f456e
madhavsGIT <kmadhav455@gamil.com>
2024-04-29 15:27:36 +0530

    sub5

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -2,24 +2,12 @@ package com.driver;
 
 public interface WaterVehicle {
     public String getVehicleName();
-
     public int getVehicleCapacity();
 
 }
 
-class Boat implements WaterVehicle{
-
-    @Override
-    public String getVehicleName() {
-        return "";
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return 0;
-    }
 
 
 
 
-}
+
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,5 +1,5 @@
 package com.driver.test;
-import com.driver.*;
+
 
 public class TestCases {
 
